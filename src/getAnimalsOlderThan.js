dc4fb99971d4a20688af63552ee0e76fe52afdb5
Victor Henrique Fernandes <victorhenriquefernandes2@gmail.com>
2023-04-18 03:20:46 -0300

    att

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,5 +1,6 @@
 const data = require('../data/zoo_data');
 
+// eslint-disable-next-line max-len
 const getAnimalsOlderThan = (animal, age) => data.species.find((animalName) => animalName.name === animal).residents.every((specie) => specie.age >= age);
 
 module.exports = getAnimalsOlderThan;
