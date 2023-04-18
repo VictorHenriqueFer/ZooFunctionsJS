519b0551794df12ecd5c0b3afa0ab506010a4886
Victor Henrique Fernandes <victorhenriquefernandes2@gmail.com>
2023-04-17 22:19:34 -0300

    adicionando testes

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,28 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+const openingHours = {
+    Tuesday: { open: 8, close: 6 },
+    Wednesday: { open: 8, close: 6 },
+    Thursday: { open: 10, close: 8 },
+    Friday: { open: 10, close: 8 },
+    Saturday: { open: 8, close: 10 },
+    Sunday: { open: 8, close: 8 },
+    Monday: { open: 0, close: 0 },
+}
+describe('Testes da função getOpeningHours', () => {
+    it('Testa o retorno da função quando chamada', () => {
+        expect(getOpeningHours()).toStrictEqual(openingHours);
+    });
+
+    it('Testa se está aberto em Monday 09:00AM', () => {
+        expect(getOpeningHours('Monday', '09:00-AM')).toBe('The zoo is closed');
+    });
+
+    it('Testa se está aberto em Tuesday 09:00AM', () => {
+        expect(getOpeningHours('Tuesday', '09:00-AM')).toBe('The zoo is open');
+    });
+
+    it('Testa se está aberto em Wednesday 09:00PM', () => {
+        expect(getOpeningHours('Wednesday', '09:00-PM')).toBe('The zoo is closed');
+    });
+});
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,57 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+    it('Retorna a quantidade de elefantes quando o argumento for "count"', () => {
+        const result = handlerElephants('count');
+        expect(result).toBeDefined();
+        expect(typeof result).toBe('number');
+    });
+
+
+    it('Retorna um array com os nomes de todos os elefantes quando o argumento for "names"', () => {
+        const result = handlerElephants('names');
+        expect(result).toBeDefined();
+        expect(Array.isArray(result)).toBe(true);
+    });
+
+    it('Retorna a média de idade dos elefantes quando o argumento for "averageAge"', () => {
+        const result = handlerElephants('averageAge');
+        expect(result).toBeDefined();
+        expect(typeof result).toBe('number');
+    });
+
+    it('Retorna a localização dos elefantes quando o argumento for "location"', () => {
+        const result = handlerElephants('location');
+        expect(result).toBeDefined();
+        expect(typeof result).toBe('string');
+    });
+
+    it('Retorna a popularidade dos elefantes quando o argumento for "popularity"', () => {
+        const result = handlerElephants('popularity');
+        expect(result).toBeDefined();
+        expect(typeof result).toBe('number');
+    });
+
+    it('Retorna um array com os dias de disponibilidade para visitação dos elefantes quando o argumento for "availability', () => {
+        const result = handlerElephants('availability');
+        expect(result).toBeDefined();
+        expect(Array.isArray(result)).toBe(true);
+    });
+
+    it('Retorna undefined quando o argumento não for uma string válida', () => {
+        const result = handlerElephants(undefined);
+        expect(result).toBeUndefined();
+    });
+
+    it('Retorna uma string de erro quando o argumento não for uma string vazia válida', () => {
+        const result = handlerElephants(123);
+        expect(result).toBeDefined();
+        expect(typeof result).toBe('string');
+    });
+
+    it('Retorna null quando o argumento não for uma opção válida', () => {
+        const result = handlerElephants('invalidOption');
+        expect(result).toBeNull();
+    });
+
+});
