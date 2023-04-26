4fce8eb5cf2473163aa1d1801aafde576b54cb1e
Victor Henrique Fernandes <victorhenriquefernandes2@gmail.com>
2023-04-26 03:23:23 -0300

    corrigindo lint

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,23 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
+const getEmployeesCoverage = (pessoa) => {
+  const getEmployees = employees.map((person) => ({
+    id: person.id,
+    fullName: `${person.firstName} ${person.lastName}`,
+    species: species.filter(({ id }) => person.responsibleFor.includes(id))
+      .map((specie) => specie.name),
+    locations: species.filter(({ id }) => person.responsibleFor.includes(id))
+      .map((specie) => specie.location),
+  }));
+  if (!pessoa || pessoa === {}) {
+    return getEmployees;
+  }
+  if (!getEmployees.some((person) => person.fullName
+    .includes(pessoa.name) || person.id === pessoa.id)) {
+    throw new Error('Informações inválidas');
+  }
+  return getEmployees.find((nome) => nome.fullName.includes(pessoa.name) || nome.id === pessoa.id);
 };
 
 module.exports = getEmployeesCoverage;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const getFirstAnimal = data.employees.find((employee) => id === employee.id)
+    .responsibleFor[0];
+  const oldest = species.find((specie) => specie.id === getFirstAnimal).residents
+    .reduce((acc, animal) => (acc.age > animal.age ? acc : animal));
+  return [oldest.name, oldest.sex, oldest.age];
 };
 
 module.exports = getOldestFromFirstSpecies;
